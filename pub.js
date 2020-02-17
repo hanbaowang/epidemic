81a776403ed3169ff36d17e3cd215ada2f559a0f
Mingfei Ding <xiesansi@gmail.com>
2020-02-17 12:15:51 +0800

    add pub test

--- pub.js
+++ pub.js
@@ -1,18 +1,19 @@
 const http = require('http');  
 
 function me(result) {
-    let url = 'YOUR SERVER';
-    serverChen(url, result)
+    let url = 'http://YOURSERVER';
+    return serverChen(url, result)
 }
 
 function somebody(result) {
-    let url = 'HER URL';
-    serverChen(url, result)
+    let url = 'http://HERURL';
+    return serverChen(url, result)
 }
 
 function serverChen(url, result, filter) {
-    if (filter && !filter(result)) return 
+    if (filter && !filter(result)) return false
     http.get(url + '?text=' + '有新的卫健委新闻' + '&desp=' + `标题: ${result.text}, 链接: ${result.href}`)
+    return true
 }
 
 function filter(result, whiteList = ["新增", "轨迹", "确诊", "治愈", "病例"], blackList = ["带队", "督导", "致敬", "坚守", "培训"]) {
