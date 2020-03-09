7ad347546b95a0606d608c4ebb174991497d7fac
Mingfei Ding <xiesansi@gmail.com>
2020-03-09 22:50:53 +0800

    new way to get the url hostname

--- crawler.js
+++ crawler.js
@@ -16,15 +16,15 @@ class Crawler {
         this.interval = params.interval;
         this.options = {
             uri: this.url,
-            headers:{
-                'Host': `wjw.${this.id}.gov.cn`,
+            headers: {
+                'Host': new URL(this.url).hostname,
                 'Connection': 'keep-alive',
                 'Cache-Control': 'max-age=0',
                 'Upgrade-Insecure-Requests': 1,
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
                 'Accept-Encoding': 'gzip, deflate',
-                'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8'  
+                'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8'
             },
             transform: function (body) {
                 return cheerio.load(body);
